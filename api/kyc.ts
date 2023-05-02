5a9b958feed83d9518449dda7d0c606df0a2880b
Work <iddan.halevy@redis.com>
2023-05-02 18:10:38 +0300

    kyc route

---
+++ api/kyc.ts
@@ -0,0 +1,17 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import { createClient } from 'redis';
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const client = createClient({
+    password: 'fOeUIS7HWAy7rbsECJQcuQn5HdjB1bvJ',
+    socket: {
+      host: 'redis-18236.internal.c13378.us-east-1-mz.ec2.cloud.rlrcp.com',
+      port: 18236
+    }
+  });
+
+  await client.connect();
+
+  const kycOptions = await client.json.get("kyc:options");
+  res.json(kycOptions);
+}
